58e8f28bea1b59e771d778d434db5c712e99142c
Gabriela <gaby@Gabrielas-MacBook-Pro.local>
2021-02-03 17:03:52 -0500

    Declare an object with parts of the message, generate a random number

---
+++ script.js
@@ -0,0 +1,11 @@
+const sentence = {
+    subject:['I', 'You' , 'We', 'They'],
+    verb :['ran', 'read', 'walk', 'dance','dont take a shower'],
+    complement : ['in the dark', 'on the bed' , 'with Carlos', 'and cry']
+};
+
+const generateRandomNumber = (num)=>{
+    return Math.floor(Math.random()*num)
+};
+
+
